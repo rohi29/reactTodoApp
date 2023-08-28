8c168b8deb9f999fae7b4edac27965d5ed95b85b
Rohi29 <rohiniphatangaren5@gmail.com>
2023-08-28 17:34:19 +0530

    added html and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <form className="todo-list">
+        <ul>
+          <div className="todo">
+            <div className="checkbox" />
+            <input type="text" value="Todo one" />
+          </div>
+        </ul>
+      </form>
     </div>
   );
 }
